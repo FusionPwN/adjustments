99ed00d0e1df63d83ecf30f7451766b38c6600d1
FusionPwN <fusionpwnu@gmail.com>
2023-09-26 16:33:08 +0100

    Update SimpleShippingFee.php

--- Adjusters/SimpleShippingFee.php
+++ Adjusters/SimpleShippingFee.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Vanilo\Adjustments\Adjusters;
 
+use App\Models\Admin\ShipmentMethod;
 use Vanilo\Adjustments\Contracts\Adjustable;
 use Vanilo\Adjustments\Contracts\Adjuster;
 use Vanilo\Adjustments\Contracts\Adjustment;
@@ -16,68 +17,69 @@ use Vanilo\Adjustments\Support\IsNotIncluded;
 
 final class SimpleShippingFee implements Adjuster
 {
-    use HasWriteableTitleAndDescription;
-    use IsLockable;
-    use IsNotIncluded;
+	use HasWriteableTitleAndDescription;
+	use IsLockable;
+	use IsNotIncluded;
 	use IsAShippingAdjusment;
 
-    private float $amount;
+	private ShipmentMethod $shipping;
+	private float $amount;
+	private ?float $freeThreshold;
 
-    private ?float $freeThreshold;
+	public function __construct(ShipmentMethod $shipping, float $amount, ?float $freeThreshold = null)
+	{
+		$this->shipping = $shipping;
+		$this->amount = $amount;
+		$this->freeThreshold = $freeThreshold;
+	}
 
-    public function __construct(float $amount, ?float $freeThreshold = null)
-    {
-        $this->amount = $amount;
-        $this->freeThreshold = $freeThreshold;
-    }
+	public static function reproduceFromAdjustment(Adjustment $adjustment): Adjuster
+	{
+		$data = $adjustment->getData();
 
-    public static function reproduceFromAdjustment(Adjustment $adjustment): Adjuster
-    {
-        $data = $adjustment->getData();
+		return new self(ShipmentMethod::find($adjustment->origin ?? 0), floatval($data['amount'] ?? 0), $data['freeThreshold'] ?? null);
+	}
 
-        return new self(floatval($data['amount'] ?? 0), $data['freeThreshold'] ?? null);
-    }
+	public function createAdjustment(Adjustable $adjustable): Adjustment
+	{
+		$adjustmentClass = AdjustmentProxy::modelClass();
 
-    public function createAdjustment(Adjustable $adjustable): Adjustment
-    {
-        $adjustmentClass = AdjustmentProxy::modelClass();
+		return new $adjustmentClass($this->getModelAttributes($adjustable));
+	}
 
-        return new $adjustmentClass($this->getModelAttributes($adjustable));
-    }
+	public function recalculate(Adjustment $adjustment, Adjustable $adjustable): Adjustment
+	{
+		$adjustment->setAmount($this->calculateAmount($adjustable));
 
-    public function recalculate(Adjustment $adjustment, Adjustable $adjustable): Adjustment
-    {
-        $adjustment->setAmount($this->calculateAmount($adjustable));
+		return $adjustment;
+	}
 
-        return $adjustment;
-    }
-
-    private function calculateAmount(Adjustable $adjustable): float
-    {
-        if (null !== $this->freeThreshold && $adjustable->itemsTotal() >= $this->freeThreshold) {
+	private function calculateAmount(Adjustable $adjustable): float
+	{
+		if (null !== $this->freeThreshold && $adjustable->itemsTotal() >= $this->freeThreshold) {
 			debug("Adding shipping free --- Cart total [" . $adjustable->itemsTotal() . "] --- Threshold [$this->freeThreshold] --- Final applied value [0]");
-            return 0;
-        }
+			return 0;
+		}
 
 		debug("Adding shipping free --- Cart total [" . $adjustable->itemsTotal() . "] --- Threshold [$this->freeThreshold] --- Final applied value [$this->amount]");
 
-        return $this->amount;
-    }
+		return $this->amount;
+	}
 
-    private function getModelAttributes(Adjustable $adjustable): array
-    {
-        return [
-            'type' 				=> AdjustmentTypeProxy::SHIPPING(),
+	private function getModelAttributes(Adjustable $adjustable): array
+	{
+		return [
+			'type' 				=> AdjustmentTypeProxy::SHIPPING(),
 			'adjustable_type' 	=> $adjustable->getMorphClass(),
 			'adjustable_id' 	=> $adjustable->id,
-            'adjuster' 			=> self::class,
-            'origin' 			=> null,
-            'title' 			=> $this->getTitle(),
-            'description' 		=> $this->getDescription(),
-            'data' 				=> ['amount' => $this->amount, 'freeThreshold' => $this->freeThreshold],
-            'amount' 			=> $this->calculateAmount($adjustable),
-            'is_locked' 		=> $this->isLocked(),
-            'is_included' 		=> $this->isIncluded(),
-        ];
-    }
+			'adjuster' 			=> self::class,
+			'origin' 			=> $this->shipping->id,
+			'title' 			=> $this->getTitle(),
+			'description' 		=> $this->getDescription(),
+			'data' 				=> ['amount' => $this->amount, 'freeThreshold' => $this->freeThreshold],
+			'amount' 			=> $this->calculateAmount($adjustable),
+			'is_locked' 		=> $this->isLocked(),
+			'is_included' 		=> $this->isIncluded(),
+		];
+	}
 }
